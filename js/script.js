0d58c1881d2518a620868df48b74fcdeab478a83
diveshthakre19 <diveshthakre07@.com>
2022-11-05 05:28:00 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const namee = document.getElementById("name");
+const date = document.getElementById("date");
+const type = document.getElementById("type");
+const amount = document.getElementById("amount");
+const table = document.querySelector("table");
+const btn = document.getElementById("addBtn");
+
+
+

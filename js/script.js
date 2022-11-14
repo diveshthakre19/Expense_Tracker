af1a0c6487b98b7114966ddf46ad53c7210cff25
diveshthakre19 <diveshthakre07@.com>
2022-11-14 08:34:28 +0530

    completed

--- js/script.js
+++ js/script.js
@@ -1,40 +1,63 @@
 const btn = document.getElementById("addBtn");
-btn.addEventListener("click", addExpense)
+btn.addEventListener("click", addExpense);
 
 // initial array of expenses, reading from localStorage
-const expenses = JSON.parse(localStorage.getItem('expenses')) || [];
+const expenses = JSON.parse(localStorage.getItem("expenses")) || [];
 
-function addExpense(){
-    let namee = document.getElementById("name").value;
-    let date = document.getElementById("date").value;
-    let type = document.getElementById("type").value;
-    let amount = document.getElementById("amount").value;
-  
-    if(type != 'chooseOne' 
-        && namee.length > 0 
-        && date != 0 
-        && amount > 0){
-            const expense = {
-                type, 
-                namee, 
-                date,
-                amount, 
-                id: expenses.length > 0 ? expenses[expenses.length - 1].id + 1 : 1,
-            }
-            
-            expenses.push(expense);
-        // localStorage 
-        localStorage.setItem('expenses', JSON.stringify(expenses));
+function addExpense() {
+  let namee = document.getElementById("name").value;
+  let date = document.getElementById("date").value;
+  let type = document.getElementById("type").value;
+  let amount = document.getElementById("amount").value;
+
+  if (type != "chooseOne" && namee.length > 0 && date != 0 && amount > 0) {
+    const expense = {
+      type,
+      namee,
+      date,
+      amount,
+      id: expenses.length > 0 ? expenses[expenses.length - 1].id + 1 : 1,
+    };
+
+    expenses.push(expense);
+    // localStorage
+    localStorage.setItem("expenses", JSON.stringify(expenses));
+  }
+  document.querySelector("form").reset();
+  showExpenses();
+}
+
+const showExpenses = () => {
+  const table = document.querySelector("tbody");
+  console.log(table);
+  table.innerHTML = " ";
+  for (let i = 0; i < expenses.length; i++) {
+    table.innerHTML += `
+        <tr>
+        <td>${i + 1}</td>
+        <td>${expenses[i].namee}</td>
+        <td>${expenses[i].date}</td>
+        <td>${expenses[i].type}</td>
+        <td>₹${expenses[i].amount}</td>
+        <td class="delete"><a class="deleteButton" onclick="deleteExpense(${expenses[i].id})"
+        >Delete
+        </td>
+        </tr>`;
+  }
+//   let editbtn = document.querySelectorAll("#editbtn");
+
+};
+
+showExpenses();
+
+const deleteExpense = (id) => {
+  for (let i = 0; i < expenses.length; i++) {
+    if (expenses[i].id == id) {
+      expenses.splice(i, 1);
     }
-    document.getElementsByClassName('form').reset();
-    showExpenses()
-}
+  }
 
-const showExpenses=()=>{
-    console.log("divesh");
-    const table = document.querySelector("table");
-    table.innerHTML=" bgvhgg"
-
-}
-
-// deleteExpense()
+  // localStorage
+  localStorage.setItem("expenses", JSON.stringify(expenses));
+  showExpenses();
+};
